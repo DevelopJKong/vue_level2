7b174edfda081c3af2c62a2bd705e5b05d5f5f76
DevelopJKong <ybaram40@gmail.com>
2021-10-23 19:14:18 +0900

    2강 완료

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
+ 
+
+export default {
+  components: {
+    //컴포넌트 태그명: 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoFooter': TodoFooter,
+    'TodoList': TodoList,
+  }
+}
+</script>
+
+<style scoped>
+  body {
+    text-align: center;
+    background: #F6F6F6;
+  }
+  input {
+    border-style: groove;
+    width:200px;
+  }
+  button {
+    border-style: groove;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+  }
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <!-- template에 들어가는 태그는 하나만 존재할수있다 -->
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function(){
+      console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem ,this.newTodoItem);
+      this.newTodoItem = '';
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
